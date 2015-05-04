87e2211a56617ae39257129d3333cf9846f770b3
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-05-04 22:32:12 +0900

    Add current code

---
+++ app.py
@@ -0,0 +1,49 @@
+import json
+from os import path
+
+import bottle
+from bottle import abort, request
+import requests
+
+
+app = bottle.default_app()
+app.config.load_config("app.conf")
+
+
+@app.route(app.config["yo.callback_url"])
+def index():
+    # Parse query
+    user_ip = request.query.user_ip
+    username = request.query.username
+    link = request.query.link
+    location = request.query.location
+
+    # Query validation
+    if user_ip == "" or username == "":
+        return abort(400)
+
+    # Message generation
+    message = "Yo from " + username
+    if link != "":
+        message += "\n" + link
+
+    # Slack notification
+    payload = {
+        "channel": app.config["slack.channel"],
+        "text": message
+    }
+
+    data = {
+        "payload": json.dumps(payload)
+    }
+
+    requests.post(app.config["slack.webhook_url"], data=data)
+
+    # Dummy response
+    return { "result": "ok" }
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+else:
+    application = app
